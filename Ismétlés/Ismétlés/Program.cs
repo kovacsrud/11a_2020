9a093b5b4e296f130125b580b38b24952398cbfb
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-11 11:41:37 +0200

    Ismétlés start

---
+++ Ismétlés/Ismétlés/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ismétlés
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Változók
+            //Egész szám típusok
+            int a = 123;
+            a = -123;
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
