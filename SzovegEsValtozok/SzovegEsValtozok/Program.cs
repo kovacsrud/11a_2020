bb9eadcc9cfdd8d40d747e3c5c6e7220ec507056
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-09-25 11:33:20 +0200

    Stringek

---
+++ SzovegEsValtozok/SzovegEsValtozok/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzovegEsValtozok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Szöveges változókkal végzett fontosabb műveletek
+            string szoveg = "Valami,bármi,akármi";
+            string szoveg2 = "valami,BÁRMI,Akármi";
+
+            //Kis- vagy nagybetűs megjelenítés
+            Console.WriteLine(szoveg.ToLower());
+            Console.WriteLine(szoveg.ToUpper());
+
+            if (szoveg.ToUpper()==szoveg2.ToUpper())
+            {
+                Console.WriteLine("Megegyeznek!");
+            }else
+            {
+                Console.WriteLine("Nem egyeznek meg!");
+                
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
