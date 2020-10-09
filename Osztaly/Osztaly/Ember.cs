86fe7a4f9e9a75ed741b69845b3b56e8c157b9b7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-10-09 11:35:03 +0200

    Példányosítás

---
+++ Osztaly/Osztaly/Ember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztaly
+{
+    public class Ember
+    {
+        public string nev;
+        public int magassag;
+        public int suly;
+        public int szulev;
+
+        public void Koszon()
+        {
+            Console.WriteLine("Hello.");
+        }
+
+    }
+}
--- Osztaly/Osztaly/Program.cs
+++ Osztaly/Osztaly/Program.cs
@@ -10,6 +10,18 @@ namespace Osztaly
     {
         static void Main(string[] args)
         {
+            Ember e1 = new Ember();
+            e1.nev = "Zénó";
+            e1.magassag = 176;
+            e1.suly = 79;
+            e1.szulev = 1998;
+
+            e1.Koszon();
+
+
+
+
+            Console.ReadKey();
         }
     }
 }
