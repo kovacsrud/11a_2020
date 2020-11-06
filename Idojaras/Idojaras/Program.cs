360bd181598f0eb9481463a5f571519a4ada1210
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-06 13:34:47 +0100

    Update Program.cs

--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -15,7 +15,7 @@ namespace Idojaras
 
             try
             {
-                var sorok = File.ReadAllLines("idojaras.csv",Encoding.Default);
+                var sorok = File.ReadAllLines("idojaras.csv", Encoding.Default);
 
                 for (int i = 1; i < sorok.Length; i++)
                 {
@@ -26,8 +26,8 @@ namespace Idojaras
                         Nap = Convert.ToInt32(e[2]),
                         Ora = Convert.ToInt32(e[3]),
                         Homerseklet = Convert.ToDouble(e[4]),
-                        Szelsebesseg=Convert.ToDouble(e[5]),
-                        Paratartalom=Convert.ToDouble(e[6])
+                        Szelsebesseg = Convert.ToDouble(e[5]),
+                        Paratartalom = Convert.ToDouble(e[6])
                     };
                     idojarasadatok.Add(adat);
                 }
@@ -35,17 +35,55 @@ namespace Idojaras
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);                
+                Console.WriteLine(ex.Message);
             }
 
             Console.WriteLine($"Sorok száma:{idojarasadatok.Count}");
 
-            var ev2016 = idojarasadatok.FindAll(x => x.Ev == 2016 && x.Honap==11);
+            var ev2016 = idojarasadatok.FindAll(x => x.Ev == 2016 && x.Honap == 11);
 
             foreach (var i in ev2016)
             {
                 Console.WriteLine($"{i.Ev}.{i.Honap}.{i.Nap} {i.Ora}");
             }
+
+            var ev2007 = idojarasadatok.Find(x => x.Ev == 2017);
+
+            if (ev2007 == null)
+            {
+                Console.WriteLine("Nincs ilyen adat!");
+            } else
+            {
+                Console.WriteLine($"{ev2007.Ev}.{ev2007.Honap}.{ev2007.Nap}");
+            }
+
+            var kiirando = idojarasadatok.FindAll(x => x.Ev == 2008);
+
+            try
+            {
+                FileStream fajl = new FileStream("ev_2008.csv", FileMode.CreateNew);
+
+                using (StreamWriter writer = new StreamWriter(fajl, Encoding.Default))
+                {
+                    writer.WriteLine("ev;honap;nap;ora;homerseklet;szelsebesseg;paratartalom");
+                    foreach (var i in kiirando)
+                    {
+                        writer.WriteLine($"{i.Ev};{i.Honap};{i.Nap};{i.Ora};{i.Homerseklet};{i.Szelsebesseg};{i.Paratartalom}");
+                    }
+                }
+                
+
+                
+                Console.WriteLine("Fájlba írás kész!");
+
+                //writer.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        
 
 
             Console.ReadKey();
