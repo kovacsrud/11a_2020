80063674fd3bd967cd11fdbd476cf63523f19a46
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-06 11:52:21 +0100

    Példányosítás példák

--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -10,7 +10,16 @@ namespace Idojaras
     {
         static void Main(string[] args)
         {
-
+            IdojarasAdat adat = new IdojarasAdat(2020, 12, 12, 22, 12.6, 13.66, 0.9);
+            IdojarasAdat adat2 = new IdojarasAdat {
+                Ev=2020,
+                Honap=11,
+                Nap=10,
+                Ora=6,
+                Homerseklet=11.4,
+                Szelsebesseg=6.2,
+                Paratartalom=0.8
+            };
 
             Console.ReadKey();
         }
