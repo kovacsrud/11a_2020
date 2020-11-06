f79726f7f0fc0f3a6a20907e2ba1d85f95070ba0
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-06 12:11:09 +0100

    Fájl betöltés

--- Idojaras/Idojaras/Program.cs
+++ Idojaras/Idojaras/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,34 @@ namespace Idojaras
     {
         static void Main(string[] args)
         {
-            IdojarasAdat adat = new IdojarasAdat(2020, 12, 12, 22, 12.6, 13.66, 0.9);
-            IdojarasAdat adat2 = new IdojarasAdat {
-                Ev=2020,
-                Honap=11,
-                Nap=10,
-                Ora=6,
-                Homerseklet=11.4,
-                Szelsebesseg=6.2,
-                Paratartalom=0.8
-            };
+            List<IdojarasAdat> idojarasadatok = new List<IdojarasAdat>();
+
+            try
+            {
+                var sorok = File.ReadAllLines("idojaras.csv",Encoding.Default);
+
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    IdojarasAdat adat = new IdojarasAdat {
+                        Ev = Convert.ToInt32(e[0]),
+                        Honap = Convert.ToInt32(e[1]),
+                        Nap = Convert.ToInt32(e[2]),
+                        Ora = Convert.ToInt32(e[3]),
+                        Homerseklet = Convert.ToDouble(e[4]),
+                        Szelsebesseg=Convert.ToDouble(e[5]),
+                        Paratartalom=Convert.ToDouble(e[6])
+                    };
+                    idojarasadatok.Add(adat);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"Sorok száma:{idojarasadatok.Count}");
 
             Console.ReadKey();
         }
