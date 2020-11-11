ff551c55191d410da9270af557c68bdb723d4028
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-11 12:47:56 +0100

    Fájl betöltés

--- StadionFeladat/StadionFeladat/Program.cs
+++ StadionFeladat/StadionFeladat/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,15 @@ namespace StadionFeladat
     {
         static void Main(string[] args)
         {
+            List<Stadion> stadionok = new List<Stadion>();
+            try
+            {
+                var sorok = File.ReadAllLines("vb2018.txt",Encoding.Default);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                 
+            }
 
             Console.ReadKey();
         }
---
+++ StadionFeladat/StadionFeladat/Stadion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StadionFeladat
+{
+    public class Stadion
+    {
+        public string Varos { get; set; }
+        public string Nev1 { get; set; }
+        public string Nev2 { get; set; }
+        public int Ferohely { get; set; }
+    }
+}
