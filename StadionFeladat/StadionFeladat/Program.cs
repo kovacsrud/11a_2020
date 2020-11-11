318078e9d85ce4d1f70f520cd6b26fbcc27945e7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-11-11 13:54:48 +0100

    Update Program.cs

--- StadionFeladat/StadionFeladat/Program.cs
+++ StadionFeladat/StadionFeladat/Program.cs
@@ -69,6 +69,10 @@ namespace StadionFeladat
                 Console.WriteLine("A város VB helyszín!");
             }
 
+            var kulonbozovaros = stadionok.ToLookup(x=>x.Varos);
+
+            Console.WriteLine($"Feladat 8: {kulonbozovaros.Count} különböző városban voltak mérkőzések");
+
             Console.ReadKey();
         }
     }
