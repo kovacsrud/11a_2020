b3a14f8d33e28c662a06bbc32e4443a771669069
szajberpiraty <szajberpiraty@gmail.com>
2020-11-20 11:54:21 +0100

    Fájl betöltés, feldolgozás

---
+++ Hegyek/Hegyek/Hegycsucs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hegyek
+{
+    public class Hegycsucs
+    {
+        public string HegycsucsNeve { get; set; }
+        public string Hegyseg { get; set; }
+        public int Magassag { get; set; }
+    }
+}
--- Hegyek/Hegyek/Program.cs
+++ Hegyek/Hegyek/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,29 @@ namespace Hegyek
     {
         static void Main(string[] args)
         {
+            List<Hegycsucs> hegycsucsok = new List<Hegycsucs>();
+            try
+            {
+                var sorok = File.ReadAllLines("hegyekMo.txt",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    hegycsucsok.Add(
+                        new Hegycsucs
+                        {
+                            HegycsucsNeve=e[0],
+                            Hegyseg=e[1],
+                            Magassag=Convert.ToInt32(e[2])
+                        }
+                        );
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);               
+            }
+
+            Console.WriteLine($"3.feladat: Hegycsúcsok száma:{hegycsucsok.Count} db");
 
             Console.ReadKey();
         }
