5228e6fbd703d5157c49f495f3107bbc8107b6b8
szajberpiraty <szajberpiraty@gmail.com>
2020-12-04 11:46:35 +0100

    Kektura start

---
+++ Kektura/Kektura/TuraSzakasz.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kektura
+{
+    public class TuraSzakasz
+    {
+        public string Kiindulopont { get; set; }
+        public string Vegpont { get; set; }
+        public double SzakaszHossz { get; set; }
+        public int EmelkedesOsszeg { get; set; }
+        public int LejtesOsszeg { get; set; }
+        public string Pecsetelohely { get; set; }
+
+    }
+}
