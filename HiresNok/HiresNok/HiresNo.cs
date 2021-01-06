7f6ae3b19c1d09881df293ed2c3c4dcb94f13f30
szajberpiraty <szajberpiraty@gmail.com>
2021-01-06 13:37:16 +0100

    Autok

---
+++ HiresNok/HiresNok/HiresNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiresNok
+{
+    public class HiresNo
+    {
+        public string Nev { get; set; }
+        public string Foglalkozas { get; set; }
+        public string Nemzetiseg { get; set; }
+
+        public string Elotag()
+        {
+            if (Nemzetiseg=="a")
+            {
+                return "Ms.";
+            } else
+            {
+                return "Frau";
+            }
+        }
+    }
+}
---
+++ HiresNok/HiresNok/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiresNok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<HiresNo> hiresnok = new List<HiresNo>();
+
+            Console.WriteLine("Akar felvinni adatot?(i/n)");
+            var felvitel = Console.ReadLine();
+
+            while (felvitel=="i")
+            {
+                Console.WriteLine("Név:");
+                var nev = Console.ReadLine();
+                Console.WriteLine("Foglalkozás:");
+                var foglalkozas = Console.ReadLine();
+                Console.WriteLine("Nemzetiség(a/n):");
+                var nemzetiseg = Console.ReadLine();
+
+                hiresnok.Add(
+                    new HiresNo
+                    {
+                        Nev=nev,
+                        Foglalkozas=foglalkozas,
+                        Nemzetiseg=nemzetiseg
+                    }
+                    );
+                
+                Console.WriteLine("Akar felvinni adatot?(i/n)");
+                felvitel = Console.ReadLine();
+
+            }
+
+            for (int i = 0; i < hiresnok.Count; i++)
+            {
+                Console.WriteLine($"{hiresnok[i].Elotag()} {hiresnok[i].Nev}," +
+                    $"{hiresnok[i].Foglalkozas}," +
+                    $"{hiresnok[i].Nemzetiseg}");
+            }
+
+            foreach (var i in hiresnok)
+            {
+                Console.WriteLine($"{i.Elotag()} {i.Nev},{i.Foglalkozas}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
