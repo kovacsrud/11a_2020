21881d1707c9ab9f31185e86202474e951975e94
szajberpiraty <szajberpiraty@gmail.com>
2021-01-15 11:34:10 +0100

    Feladat 1.

---
+++ AgazatiSor/AgazatiSor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgazatiSor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot:");
+            var szam = Convert.ToInt32(Console.ReadLine());
+
+            if (szam>0)
+            {
+                Console.WriteLine("Ez a szám pozitív!");
+            }
+            else if (szam<0)
+            {
+                Console.WriteLine("Ez a szám negatív!");
+            } else
+            {
+                Console.WriteLine("A szám nulla!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
