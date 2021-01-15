701505fb575fdd7aa470ca9d51db8b4699d3761b
szajberpiraty <szajberpiraty@gmail.com>
2021-01-15 11:49:32 +0100

    2.feladat

---
+++ AgazatiSor/Homerseklet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homerseklet
+{
+    class Program
+    {
+        static double Celsius2Fahrenheit(double celsius)
+        {
+            var fahrenheit = (celsius * 1.8) + 32;
+            return fahrenheit;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy hőmérsékletet celsiusban:");
+            var celsius = Convert.ToDouble(Console.ReadLine());
+
+            while (celsius!=-111)
+            {
+                //Függvény hívása
+                Console.WriteLine($"{celsius} az {Celsius2Fahrenheit(celsius)} fahrenheit");
+                Console.Write("Adj meg egy hőmérsékletet celsiusban:");
+                celsius = Convert.ToDouble(Console.ReadLine());
+            }
+
+
+            Console.ReadKey();
+        }
+
+    }
+}
