94b621b909006e5ec7ea67fbb0f69f5079c83417
szajberpiraty <szajberpiraty@gmail.com>
2021-01-15 12:08:15 +0100

    Feladat 3 feltöltés

---
+++ AgazatiSor/Munkavallalo/Dolgozo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munkavallalo
+{
+    public class Dolgozo
+    {
+        public string Nev { get; set; }
+        public string Munkakor { get; set; }
+        public int Szuletesiev { get; set; }
+
+        public int Eletkor()
+        {
+            var datum = DateTime.Now;
+            return datum.Year - Szuletesiev;
+        }
+
+    }
+}
---
+++ AgazatiSor/Munkavallalo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Munkavallalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozo> dolgozok = new List<Dolgozo>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Név:");
+                var nev = Console.ReadLine();
+
+                Console.Write("Munkakör:");
+                var munkakor = Console.ReadLine();
+
+                Console.Write("Születési év:");
+                var szulev = Convert.ToInt32(Console.ReadLine());
+
+                Dolgozo dolgozo = new Dolgozo
+                {
+                    Nev = nev,
+                    Munkakor=munkakor,
+                    Szuletesiev=szulev
+                };
+                dolgozok.Add(dolgozo);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
