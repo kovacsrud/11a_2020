cc97a70d7422e8cd72a4e4d63913598b046d8904
szajberpiraty <szajberpiraty@gmail.com>
2021-01-29 11:45:17 +0100

    Snooker start

---
+++ Snooker/Snooker/Versenyzo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snooker
+{
+    public class Versenyzo
+    {
+        public int Helyezes { get; set; }
+        public string Nev { get; set; }
+        public string Orszag { get; set; }
+        public int Nyeremeny { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            var e = sor.Split(';');
+            Helyezes = Convert.ToInt32(e[0]);
+            Nev = e[1];
+            Orszag = e[2];
+            Nyeremeny = Convert.ToInt32(e[3]);
+        }
+    }
+}
