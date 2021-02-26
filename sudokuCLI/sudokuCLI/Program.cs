4c6db90c778c106d0d9bc42d1da75c024b9143a3
szajberpiraty <szajberpiraty@gmail.com>
2021-02-26 11:58:55 +0100

    Beolvasás

---
+++ sudokuCLI/sudokuCLI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sudokuCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Feladvany> feladvanyok = new List<Feladvany>();
+
+            try
+            {
+                var sorok = File.ReadAllLines("feladvanyok.txt",Encoding.Default);
+
+                for (int i = 0; i < sorok.Length; i++)
+                {
+                    feladvanyok.Add(new Feladvany(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);               
+            }
+
+            Console.WriteLine($"3.feladat: Beolvasva {feladvanyok.Count} feladvány");
+
+            Console.ReadKey();
+        }
+    }
+}
