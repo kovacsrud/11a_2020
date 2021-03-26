fce22ead168b6923b67fcd39cafec9d8435cf6c7
szajberpiraty <szajberpiraty@gmail.com>
2021-03-26 13:31:02 +0100

    Update Program.cs

--- CSUDH/CSUDH/Program.cs
+++ CSUDH/CSUDH/Program.cs
@@ -34,6 +34,53 @@ namespace CSUDH
                 Console.WriteLine($"{i+1}.szint:{elso.GetDomain(i+1)}");
             }
 
+            string htmlFajl = "<HTML>\n";
+            htmlFajl += "<HEAD>\n";
+            htmlFajl += "<TITLE>Domain nevek</TITLE>\n";
+            htmlFajl += "</HEAD>\n";
+            htmlFajl += "<BODY>\n";
+            htmlFajl += "<TABLE>\n";
+            htmlFajl += "<TR>\n";
+            htmlFajl += "<TH style='text-align:left'>Ssz</TH>\n";
+            htmlFajl += "<TH style='text-align:left'>Host domain neve</TH>\n";
+            htmlFajl += "<TH style='text-align:left'>Host IP címe</TH>\n";
+            
+            for (int i = 0; i < 5; i++)
+            {
+                htmlFajl += $"<TH style='text-align:left'>{i+1}.szint</TH>\n";
+            }
+            htmlFajl += "</TR>\n";
+
+            //innentől jönnek az adatok
+            int sorszam = 1;
+            foreach (var j in domains)
+            {
+                htmlFajl += "<TR>\n";
+                htmlFajl += $"<TD style='text-align:left'>{sorszam}</TD>\n";
+                htmlFajl += $"<TD style='text-align:left'>{j.DomainName}</TD>\n";
+                htmlFajl += $"<TD style='text-align:left'>{j.IpAddress}</TD>\n";
+                for (int i = 0; i < 5; i++)
+                {
+                    htmlFajl += $"<TD style='text-align:left'>{j.GetDomain(i+1)}</TD>\n";
+                }
+
+                htmlFajl += "</TR>\n";
+                sorszam++;
+            }
+            htmlFajl += "</TABLE>\n";
+
+            htmlFajl += "</BODY>\n";
+            htmlFajl += "</HTML>\n";
+
+            try
+            {
+                File.WriteAllText("table.html",htmlFajl);
+                Console.WriteLine("Kiírás kész!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
 
 
             Console.ReadKey();
