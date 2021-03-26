5a597132f07fd741f6b5d671fed955d860930116
szajberpiraty <szajberpiraty@gmail.com>
2021-03-26 11:53:37 +0100

    Domainek száma

--- CSUDH/CSUDH/Program.cs
+++ CSUDH/CSUDH/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace CSUDH
     {
         static void Main(string[] args)
         {
+            List<Domain> domains = new List<Domain>();
 
+            try
+            {
+                var sorok = File.ReadAllLines("csudh.txt", Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    domains.Add(new Domain(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"3.feladat: Domainek száma:{domains.Count}");
 
             Console.ReadKey();
         }
