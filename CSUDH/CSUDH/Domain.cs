a67f2964f7394e42977cf50f921166847827bace
szajberpiraty <szajberpiraty@gmail.com>
2021-03-26 11:47:39 +0100

    Csudh start

---
+++ CSUDH/CSUDH/Domain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSUDH
+{
+    public class Domain
+    {
+        public string DomainName { get; set; }
+        public string IpAddress { get; set; }
+
+        public Domain(string sor)
+        {
+            var e = sor.Split(';');
+            DomainName = e[0];
+            IpAddress = e[1];
+        }
+    }
+}
