fb6561a60bcbb1ff21c4407c481e9dc7b1afb92b
szajberpiraty <szajberpiraty@gmail.com>
2021-04-13 08:29:00 +0200

    Update Program.cs

--- Pilotak/Pilotak/Program.cs
+++ Pilotak/Pilotak/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,21 @@ namespace Pilotak
     {
         static void Main(string[] args)
         {
+            List<Pilota> pilotak = new List<Pilota>();
+            try
+            {
+                var sorok = File.ReadAllLines("pilotak.csv",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    pilotak.Add(new Pilota(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"3.feladat:{pilotak.Count}");
 
             Console.ReadKey();
         }
