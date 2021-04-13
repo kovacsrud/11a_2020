85c5d982dbe8ab619b88d263a61ec2bcbfffce68
szajberpiraty <szajberpiraty@gmail.com>
2021-04-13 09:06:08 +0200

    Update Program.cs

--- Pilotak/Pilotak/Program.cs
+++ Pilotak/Pilotak/Program.cs
@@ -27,6 +27,25 @@ namespace Pilotak
 
             Console.WriteLine($"3.feladat:{pilotak.Count}");
 
+            Console.WriteLine($"4.feladat:{pilotak.Last().Nev}");
+            //Alternatív megoldás az utolsó elem kiválasztására
+            Console.WriteLine($"4.feladat:{pilotak[pilotak.Count-1].Nev}");
+
+            var regiPilotak = pilotak.FindAll(x=>x.SzuletesiEv<1901);
+
+            Console.WriteLine("5.feladat");
+            foreach (var i in regiPilotak)
+            {
+                Console.WriteLine($"{i.Nev} ({i.SzuletesiEv}.{i.SzuletesiHo}.{i.SzuletesiNap}.)");
+            }
+
+            var rajtszamosPilotak = pilotak.FindAll(x=>x.Rajtszam!=-11);
+
+            var legkisebbRajtszam = rajtszamosPilotak.Find(x=>x.Rajtszam==rajtszamosPilotak.Min(y=>y.Rajtszam));
+            
+            Console.WriteLine($"6.feladat:{legkisebbRajtszam.Nemzetiseg}");
+            
+
             Console.ReadKey();
         }
     }
