c59255abad588b0030877dfad4adf04fdf9f9522
szajberpiraty <szajberpiraty@gmail.com>
2021-04-23 13:43:44 +0200

    Balkezesek feladat

---
+++ Balkezesek/Balkezesek/Dobo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    public class Dobo
+    {
+        public string Nev { get; set; }
+        public DateTime Elso { get; set; }
+        public DateTime Utolso { get; set; }
+        public int Suly { get; set; }
+        public int Magassag { get; set; }
+
+        public Dobo(string sor)
+        {
+            var e = sor.Split(';');
+            Nev = e[0];
+            Elso = DateTime.Parse(e[1]);
+            Utolso = DateTime.Parse(e[2]);
+            Suly = Convert.ToInt32(e[3]);
+            Magassag = Convert.ToInt32(e[4]);
+        }
+    }
+}
---
+++ Balkezesek/Balkezesek/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dobo> dobok = new List<Dobo>();
+
+            try
+            {
+                var sorok = File.ReadAllLines("balkezesek.csv",Encoding.Default);
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    dobok.Add(new Dobo(sorok[i]));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);                
+            }
+
+            Console.WriteLine($"Adatok száma:{dobok.Count}");
+
+            var okt99 = dobok.FindAll(x=>x.Utolso.Year==1999 && x.Utolso.Month==10);
+
+            foreach (var i in okt99)
+            {
+                Console.WriteLine($"{i.Nev}-{i.Magassag*2.54:0.0}");
+            }
+
+            Console.Write("Adjon meg egy évszámot:");
+            var evszam = Convert.ToInt32(Console.ReadLine());
+
+            while (evszam<1990 || evszam>1999)
+            {
+                Console.Write("Hibás érték, újra:");
+                evszam = Convert.ToInt32(Console.ReadLine());
+            }
+         
+
+
+            Console.ReadKey();
+        }
+    }
+}
